2056156f51cd4051eaec14075a6ff012b46e3438
CrioUser <crio-user@criodo.com>
2025-11-16 19:53:33 +0530

    feat: add home endpoint and configure application logging using logback.xml

--- src/main/java/com/crio/learning_navigator/LearningNavigatorApplication.java
+++ src/main/java/com/crio/learning_navigator/LearningNavigatorApplication.java
@@ -1,13 +1,25 @@
 package com.crio.learning_navigator;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@Slf4j
 @SpringBootApplication
+@RestController
 public class LearningNavigatorApplication {
 
 	public static void main(String[] args) {
+		log.info("LEARNING NAVIGATOR APPLICATION STARTS!!!");
 		SpringApplication.run(LearningNavigatorApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String home() {
+		log.info("LEARNING NAVIGATOR APPLICATION works gracefully");
+		return "WELCOME TO LEARNING NAVIGATOR APPLICATION";
+	}
 }
